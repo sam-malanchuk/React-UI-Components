567324bafc81cccbc41ff69b04174ab430fde758
sam-malanchuk <malanchuksam@gmail.com>
2019-05-29 21:08:47 -0400

    completed footer component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
+import FooterContainer from './components/FooterComponents/Footer';
 
 const App = () => {
   return (
     <div className="app">
       <HeaderContainer />
       <CardContainer />
-        <div className="footerContainer">
-
-        </div>
-
+      <FooterContainer />
     </div>
   );
 };
